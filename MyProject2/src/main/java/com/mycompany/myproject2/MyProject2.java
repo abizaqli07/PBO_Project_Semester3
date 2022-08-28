5c29851362c0c6e38f4fd73913e3b2b6970d0084
abikhoir07 <abikhoir42@gmail.com>
2022-08-28 20:24:33 +0700

    first project
    first project

---
+++ MyProject2/src/main/java/com/mycompany/myproject2/MyProject2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.myproject2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MyProject2 {
+
+    public static void main(String[] args) {
+        System.out.println("      A        BBBBB    I");
+        System.out.println("     AAA       B   BB   I");
+        System.out.println("    AA AA      B  BB    I");
+        System.out.println("   AA   AA     BBBB     I");
+        System.out.println("  AAAAAAAAA    B  BB    I");
+        System.out.println(" AA       AA   B   BB   I");
+        System.out.println("AA         AA  BBBBB    I");
+    }
+}
